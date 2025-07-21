5a92c08e216f6b4e2a88ecaf7343fe3426485296
Lucas de Castro <lucas.castro.souza@al.infnet.edu.br>
2025-07-21 20:34:22 -0300

    AccessToken Logic

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,8 +2,11 @@ import { LogOutIcon } from "lucide-react-native";
 import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { colors } from "../styles/colors";
+import { useAuth } from "../hooks/useAuth";
 
 export function HomeHeader() {
+  const {signOut} = useAuth()
+
   return (
     <View className='bg-lime-400 h-[130px]'>
       <SafeAreaView className="px-4 flex-row items-center justify-between">
@@ -11,7 +14,7 @@ export function HomeHeader() {
           <Text className="text-gray-700 text-sm font-sans-regular">Olá!</Text>
           <Text className="text-black-700 text-base font-sans-semibold">Lucas</Text>
         </View>
-        <TouchableOpacity className="size-12 items-center justify-center">
+        <TouchableOpacity className="size-12 items-center justify-center" onPress={signOut}>
           <LogOutIcon size={20} color={colors.black[700]}/>
         </TouchableOpacity>
       </SafeAreaView>
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
-import { createContext } from 'react';
+import { createContext, useCallback, useEffect, useState } from 'react';
 import { httpClient } from '../services/httpClient';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 type SignInParams = {
   email: string;
@@ -26,30 +27,69 @@ interface IAuthContextValue {
   isLoading: boolean;
   signIn(params: SignInParams): Promise<void>;
   signUp(params: SignUpParams): Promise<void>;
+  signOut(): Promise<void>;
 }
 
 export const AuthContext = createContext({} as IAuthContextValue);
 
+const TOKEN_STORAGE_KEY = 'user::token'
+
 export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [token, setToken] = useState<string | null>(null);
+  const [isLoadingToken, setIsLoadingToken] = useState(true);
+
+  // Verifica se existe um token salvo no Storage e carrega ele
+  useEffect(() => {
+    async function load() {
+      const data = await AsyncStorage.getItem(TOKEN_STORAGE_KEY)
+      setToken(data);
+      setIsLoadingToken(false)
+    }
+
+    load()
+  }, [])
+
+  // Lida com a persistência do token
+  useEffect(() => {
+    async function run() { // está sendo usado o async para executar o AsyncStorage
+      // Se o token existir, adiciona ele no storage
+      if (!token) {
+        return;
+      }
+
+      await AsyncStorage.setItem(TOKEN_STORAGE_KEY, token)
+    }
+
+    run()
+  }, [token])
+
   const { mutateAsync: signIn } = useMutation({
     mutationFn: async (params: SignInParams) => {
       const { data } = await httpClient.post('/signin', params);
+      setToken(data.accessToken)
     },
   });
   
   const { mutateAsync: signUp } = useMutation({
     mutationFn: async (params: SignUpParams) => {
       const { data } = await httpClient.post('/signup', params);
+      setToken(data.accessToken)
     },
   });
+
+  const signOut = useCallback(async () => {
+    setToken(null);
+    await AsyncStorage.removeItem(TOKEN_STORAGE_KEY)
+  }, [])
 
   return (
     <AuthContext.Provider
       value={{
-        isLoggedIn: false,
-        isLoading: false,
+        isLoggedIn: !!token, // !! -> Retorna true se não for null
+        isLoading: isLoadingToken, // Mantem o usuário na Splash enquanto o token não foi verificado.
         signIn,
         signUp,
+        signOut
       }}
     >
       {children}
